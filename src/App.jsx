427269b1587faefbb5be69aea81cc7ada2507365
Cuauhtemoc Rodriguez <1979cuau@gmail.com>
2023-05-21 13:11:23 -0600

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import CardUser from "./components/CardUser";
+
+function App() {
+
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    const URL = 'https://randomuser.me/api/'
+    axios.get(URL)
+      .then(res => setUser(res.data.results[0]))
+      .catch(err => console.log(err))
+  }, [])
+  
+  console.log(user)
+
+  return (
+    <div className="app">
+            <h1>RANDOM USER GENERATOR</h1>
+      <p className="description">
+        A free,
+        <a href="https://randomuser.me/">open-source </a>
+        API for generating random user data
+      </p>
+      <div className="card__user">
+      <CardUser user={user}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardUser.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../App.css";
+
+const CardUser = ({ user }) => {
+  return (
+    <section className="card__section">
+      <div className="card__name">
+        <img src={user?.picture.large} alt="" />
+        <h2>
+        {`${user?.name.title} ${user?.name.first} ${user?.name.last}`}
+        </h2>
+      </div>
+      <hr />
+      <div className="card__description">
+      <ul>
+        <li>
+          <span>Email: </span>
+          {user?.email}
+        </li>
+        <li>
+          <span>Gender: </span>
+          {user && user.gender}
+        </li>
+        <li>
+          <span>Location: </span>
+          {`${user?.location.country}, ${user?.location.city}`}
+        </li>
+      </ul>
+      </div>
+      <hr />
+      <div className="card__btn">
+        <a href="http://localhost:5174/">New user random</a>
+      </div>
+    </section>
+  );
+};
+
+export default CardUser;
